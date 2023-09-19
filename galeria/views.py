d91fb36cf57997915a07ef2d3dcd1e804f3c42db
Israel Veras <israelveras@gmail.com>
2023-09-18 22:05:48 -0300

    v2

--- galeria/views.py
+++ galeria/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 # from django.http import HttpResponse
+from galeria.models import Fotografia
+
 
 def index(request):
-    return render(request, 'galeria/index.html')
+    fotografias = Fotografia.objects.filter(publicada=True).order_by("-data_fotografia")
+    return render(request, 'galeria/index.html', {"cards": fotografias})
+
+def imagem(request, foto_id):
+    fotografia = get_object_or_404(Fotografia, pk=foto_id)
+    return render(request, 'galeria/imagem.html', {"fotografia": fotografia})
