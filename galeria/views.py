a268e22c2831773e6203a372ed1444afbda4d0ba
Israel Veras <israelveras@gmail.com>
2023-12-29 11:00:52 -0300

    usuarios

--- galeria/views.py
+++ galeria/views.py
@@ -1,17 +1,25 @@
-from django.shortcuts import render, get_object_or_404
-# from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib import messages
 from galeria.models import Fotografia
 
 
 def index(request):
-    fotografias = Fotografia.objects.filter(publicada=True).order_by("-data_fotografia")
-    return render(request, 'galeria/index.html', {"cards": fotografias})
+    if request.user.is_authenticated:
+        fotografias = Fotografia.objects.filter(publicada=True).order_by("-data_fotografia")
+        return render(request, 'galeria/index.html', {"cards": fotografias})
+    else:
+        messages.error(request, "Usuário não logado")
+        return redirect('login')
 
 def imagem(request, foto_id):
     fotografia = get_object_or_404(Fotografia, pk=foto_id)
     return render(request, 'galeria/imagem.html', {"fotografia": fotografia})
 
 def buscar(request):
+    if not request.user.is_authenticated:
+        messages.error(request, 'Para efetuar buscas, o usuário deve estar logado')
+        return redirect('login')
+
     fotografias = Fotografia.objects.filter(publicada=True).order_by("-data_fotografia")
     if "buscar" in request.GET:
         nome_a_buscar = request.GET['buscar']
